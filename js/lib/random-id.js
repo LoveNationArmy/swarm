1111d8fc49ed86b0d9f769dddceb10addf23c83d
George Stagas <gstagas@gmail.com>
2020-05-26 01:13:56 +0400

    random-id: unnamed default export

--- js/lib/random-id.js
+++ js/lib/random-id.js
@@ -1,2 +1,1 @@
-const randomId = (n = 16) => Array(n).fill().map(() => (16*Math.random()|0).toString(16)).join``
-export default randomId
+export default (n = 16) => Array(n).fill().map(() => (16*Math.random()|0).toString(16)).join``
