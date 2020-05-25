62970dc17aec417e1e8d91b04787628b650a4f45
George Stagas <gstagas@gmail.com>
2020-05-26 00:23:21 +0400

    add message

---
+++ js/message.js
@@ -0,0 +1,33 @@
+export default class Message extends Object {
+  static parse (message) {
+    if (!message.length) {
+      throw TypeError('Message.parse error: Message empty')
+    }
+    try { message = JSON.parse(message) } catch (_) {}
+    const metaIndex = message.indexOf` `
+    let [type, from, to, id, time] = message.slice(0, metaIndex).split`:`.reverse()
+    let data = message.slice(metaIndex + 1)
+    if (metaIndex < 0) type = data, data = undefined
+    else try { data = JSON.parse(data) } catch (_) {}
+    return { time, id, to, from, type, data }
+  }
+
+  static serialize (message) {
+    if (typeof message === 'string') return message
+    const { time, id, to, from, type, data } = message
+    const meta = [time, id, to, from, type].filter(Boolean).join`:`
+    const json = JSON.stringify(data)
+    return [meta, json].join` `
+  }
+
+  constructor (obj) {
+    super()
+    if (typeof obj === 'string') obj = Message.parse(obj)
+    if (typeof obj === 'object') Object.assign(this, obj)
+    else throw TypeError(`Message neither string or object: ${obj}`)
+  }
+
+  toString () {
+    return Message.serialize(this)
+  }
+}
---
+++ test/unit/message.js
@@ -0,0 +1,173 @@
+import Message from '../../js/message.js'
+
+describe('new Message', function () {
+  it('extends Object', () => {
+    const msg = new Message({})
+    expect(msg).to.be.an('object')
+    expect(msg).to.deep.equal({})
+  })
+
+  it('receives initial object', () => {
+    const msg = new Message({ foo: 'bar' })
+    expect(msg).to.deep.equal({ foo: 'bar' })
+  })
+
+  it('throws on undefined', () => {
+    try {
+      new Message()
+    } catch (error) {
+      expect(error.message).to.be.string('undefined')
+    }
+  })
+})
+
+describe('Message.parse', function () {
+  it('parses a Message string', () => {
+    const fixture = 'time:id:to:from:type {"foo":"bar"}'
+    const expected = {
+      time: 'time',
+      id: 'id',
+      to: 'to',
+      from: 'from',
+      type: 'type',
+      data: { foo: 'bar' }
+    }
+    const obj = Message.parse(fixture)
+    expect(obj).to.deep.equal(expected)
+  })
+
+  it('forgives data as string', () => {
+    const fixture = 'time:id:to:from:type non json string'
+    const expected = {
+      time: 'time',
+      id: 'id',
+      to: 'to',
+      from: 'from',
+      type: 'type',
+      data: 'non json string'
+    }
+    const obj = Message.parse(fixture)
+    expect(obj).to.deep.equal(expected)
+  })
+
+  it('forgives right to left', () => {
+    let fixture, expected, obj
+
+    fixture = 'type data'
+    expected = {
+      time: undefined,
+      id: undefined,
+      to: undefined,
+      from: undefined,
+      type: 'type',
+      data: 'data'
+    }
+    obj = Message.parse(fixture)
+    expect(obj).to.deep.equal(expected)
+
+    fixture = 'from:type data'
+    expected = {
+      time: undefined,
+      id: undefined,
+      to: undefined,
+      from: 'from',
+      type: 'type',
+      data: 'data'
+    }
+    obj = Message.parse(fixture)
+    expect(obj).to.deep.equal(expected)
+  })
+
+  it('single word is just type', () => {
+    const fixture = 'foo'
+    const expected = {
+      time: undefined,
+      id: undefined,
+      to: undefined,
+      from: undefined,
+      type: 'foo',
+      data: undefined
+    }
+    const obj = Message.parse(fixture)
+    expect(obj).to.deep.equal(expected)
+  })
+
+  it('empty throws', () => {
+    const fixture = ''
+    try {
+      const obj = Message.parse(fixture)
+    } catch (error) {
+      expect(error.message).to.be.string('Message.parse error')
+    }
+  })
+})
+
+
+describe('Message.serialize', function () {
+  it('serializes a Message object', () => {
+    const fixture = {
+      time: 'time',
+      id: 'id',
+      to: 'to',
+      from: 'from',
+      type: 'type',
+      data: { foo: 'bar' }
+    }
+    const expected = 'time:id:to:from:type {"foo":"bar"}'
+    const string = Message.serialize(fixture)
+    expect(string).to.deep.equal(expected)
+  })
+
+  it('forgives missing fields', () => {
+    const fixture = {
+      time: undefined,
+      id: undefined,
+      to: undefined,
+      from: undefined,
+      type: 'type',
+      data: 'data'
+    }
+    const expected = 'type "data"'
+    const string = Message.serialize(fixture)
+    expect(string).to.deep.equal(expected)
+  })
+
+  it('string object simply returns as is ', () => {
+    const fixture = 'foo'
+    const expected = 'foo'
+    const string = Message.serialize(fixture)
+    expect(string).to.equal(expected)
+  })
+})
+
+describe('new Message(string)', function () {
+  it('parses', () => {
+    const fixture = 'time:id:to:from:type {"foo":"bar"}'
+    const expected = {
+      time: 'time',
+      id: 'id',
+      to: 'to',
+      from: 'from',
+      type: 'type',
+      data: { foo: 'bar' }
+    }
+    const obj = new Message(fixture)
+    expect(obj).to.deep.equal(expected)
+  })
+})
+
+describe('message.toString()', function () {
+  it('return serialized', () => {
+    const fixture = {
+      time: 'time',
+      id: 'id',
+      to: 'to',
+      from: 'from',
+      type: 'type',
+      data: { foo: 'bar' }
+    }
+    const expected = 'time:id:to:from:type {"foo":"bar"}'
+    const string = new Message(fixture).toString()
+    expect(string).to.deep.equal(expected)
+  })
+})
