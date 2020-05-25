ddb4e27459c50444821cfb5b0b729463ac265ad4
George Stagas <gstagas@gmail.com>
2020-05-25 23:36:33 +0400

    test: peer improve coverage

--- test/unit/peer.js
+++ test/unit/peer.js
@@ -2,6 +2,16 @@ import { emit, once } from '../../js/lib/events.js'
 import Peer from '../../js/peer.js'
 
 describe('Peer', function () {
+  it('use default options', () => {
+    const peer = new Peer()
+    expect(peer.getConfiguration().iceServers.length).to.equal(2)
+  })
+
+  it('override options', () => {
+    const peer = new Peer({})
+    expect(peer.getConfiguration().iceServers.length).to.equal(0)
+  })
+
   it('has id', () => {
     const peer = new Peer({})
     expect(peer.id).to.be.a('string')
@@ -15,10 +25,12 @@ describe('Peer', function () {
     const maybeDone = () => --count || done()
 
     once(alice, 'message', message => {
+      expect(message.id).to.equal(alice.id)
       emit(bob, message.type, message)
     })
 
     once(bob, 'message', message => {
+      expect(message.id).to.equal(bob.id)
       emit(alice, message.type, message)
     })
 
